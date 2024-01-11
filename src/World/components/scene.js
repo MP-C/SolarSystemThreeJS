f93073b8f97785cfac2723d2ac4d76f207354062
MP-C <mp.crcarvalho@gmail.com>
2024-01-11 23:51:19 +0100

    correction of scene.js

--- src/World/components/scene.js
+++ src/World/components/scene.js
@@ -1,13 +1,14 @@
+console.log("scene file.js");
+
 import * as THREE from 'three';
 
-/* Para criar a cena que, serve como zona de atualização de cada função quando existem
- novas formas / objetos a serem inseridos para que sejam vísiveis no World.js */
-
+/* To create the scene that serves as an update zone for each function when there are
+new shapes/objects to be inserted so that they are visible in main.js */
 function createScene() {
     const scene = new THREE.Scene();
+    //scene.background = new THREE.Color('#b3b3b3');
 
-    scene.background = new THREE.Color('#b3b3b3');
-
+    
     return scene;
 }
 
