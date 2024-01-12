d3ca7b3a6567788603ac796626c00763046a917f
MP-C <mp.crcarvalho@gmail.com>
2024-01-12 01:36:14 +0100

    correction of renderer.js

--- src/World/system/renderer.js
+++ src/World/system/renderer.js
@@ -2,14 +2,14 @@ console.log("renderer file.js");
 
 import * as THREE from 'three';
 
-/* Para que se possa atualizar de forma constante o ficheiro assim que ocorra 
-uma modificação dinâmica em qualquer parte do projeto */
+/* To be able to constantly update the file as soon as a dynamic modification occurs
+    in any part of the project*/
 function createRenderer() {
 
     const renderer = new THREE.WebGLRenderer();
-
-    /* Para atualizar o pixel (dimensão, espaço, aspeto, ...) no global (index) 
-    em que se torna visual o universo criado */
+  
+    /* To update the pixel (dimension, space, aspect, ...) in the global (index)
+    in which the created universe becomes visible*/
     renderer.setPixelRatio( window.devicePixelRatio );
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
