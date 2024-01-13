1160a2d95c80d07cd423fb0c587b966f151dbb93
MP-C <mp.crcarvalho@gmail.com>
2024-01-13 14:28:28 +0100

    new planet.js

--- src/World/components/planet.js
+++ src/World/components/planet.js
@@ -6,9 +6,17 @@ function createPlanet(texture, a,b,c, ring ) {
     const textureLoad = new THREE.TextureLoader().load(texture);
 
     const planetGeometry = new THREE.SphereGeometry(a,b,c);
-    const planetMaterial = new THREE.MeshStandardMaterial({
+    const planetMaterial = new THREE.MeshStandardMaterial({ // MeshPhongMaterial // MeshStandardMaterial // MeshBasicMaterial
         map: textureLoad
+        // color: '0xFF0'
     })
+    // const sunGeometry = new THREE.SphereGeometry(5,46,99);
+    // const sunMaterial = new THREE.MeshBasicMaterial({ 
+    //     map: loader.load('../src/img/sun.jpg')
+    // });
+    // const sun1 = new THREE.Mesh(sunGeometry, sunMaterial);
+    // scene.add(sun1);
+
 
     // To add a ring to Saturn
     if(ring){
