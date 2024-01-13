83beba87e17fcdb2a663cf5fab6b8ce978f6f231
MP-C <mp.crcarvalho@gmail.com>
2024-01-13 14:30:55 +0100

    new sky but not working

--- src/World/components/skyHorizont.js
+++ src/World/components/skyHorizont.js
@@ -6,16 +6,25 @@ import {galaxyTexture} from '../../img/galaxy.jpg';
 import {galaxyZoneTexture} from '../../img/galaxyZone.jpg';
 
 function createSkyHorizont() {
-    const galaxy = new THREE.TextureLoader().load(galaxyZoneTexture);
+    //const galaxy = new THREE.TextureLoader().load(galaxyZoneTexture);
     //creating a cube 
     var geometry = new THREE.BoxGeometry(1,1,1);
-    var material = new THREE.MeshBasicMaterial(/*{color:"black", transparent: "true"} */).galaxy;
+    var material = new THREE.MeshBasicMaterial({color:"yellow", transparent: "true"}).galaxy;
+    
+    
+    const texture = new THREE.TextureLoader().load(galaxyTexture); 
+    // immediately use the texture for material creation 
+    // const material = new THREE.MeshStandardMaterial( {
+    //     map:texture,
+    //     side:THREE.BackSide
+    // } );
     
     var sky = new THREE.Mesh(geometry, material);
+
     //changing size of cube which is created.
-    sky.scale.x = 100;
-    sky.scale.y = 100;
-    sky.scale.z = 100;
+    // sky.scale.x = 100;
+    // sky.scale.y = 100;
+    // sky.scale.z = 100;
 
     // const space = new THREE.Mesh(boxGeo, boxMat)
     // const space = new THREE.CubeTextureLoader()
