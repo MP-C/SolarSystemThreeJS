95f2105da11e59c5258f64e9e1e42fa2e599c42f
MP-C <mp.crcarvalho@gmail.com>
2024-01-13 14:32:16 +0100

    clean main.js

--- src/main.js
+++ src/main.js
@@ -3,8 +3,7 @@ console.log("main file.js");
 import * as THREE from 'three';
 
 /// TODO: to add with timeline when page opened
-// import "../src/style/globalStyle.css";
-// import gsap from 'gsap';
+import gsap from 'gsap';
 
 // Importing files from ./components
 import { createScene } from '../src/World/components/scene.js';
@@ -42,11 +41,8 @@ import {uranusRingTexture} from '../src/img/uranusring.jpg';
 import {neptunoTexture} from '../src/img/neptuno.jpg';
 
 
-// const rendered = new THREE.WebGLRenderer();
-// rendered.setSize(window.innerWidth, window.innerHeight);
-// document.body.appendChild(rendered.domElement);
 const rendered = createRenderer();
-const scene = createScene();    // const scene  = new THREE.Scene();
+const scene = createScene();
 const camera = createCamera();
 
 // To control the object smoothly
@@ -58,67 +54,100 @@ controls.autoRotate = true;
 controls.rotateSpeed = 1;
 controls.update();
 
+const solarSystemGroup = new THREE.Group();
 
-const stars = starfields();
-scene.add(stars);
-
-const loader = new THREE.TextureLoader();
-const backgroundImage = loader.load('https://images.pexels.com/photos/1205301/pexels-photo-1205301.jpeg');
-scene.add(backgroundImage);
-
-const sky = createSkyHorizont();
-scene.background = sky;
-// space.position.set(5,5,5);
-scene.add(sky);
-
-const ambientLight = new THREE.AmbientLight(0xffffff, 1)
-scene.add(ambientLight)
+// light to see the scene
+const ambientLight = new THREE.AmbientLight(0xffffff, 1.9,100)
+solarSystemGroup.add(ambientLight)
 
 const light = new THREE.PointLight(0xffffff,1,300)
 light.position.set(0,0,0);
-scene.add(light);
+solarSystemGroup.add(light);
 
-const axes = new THREE.AxesHelper(10);
-scene.add(axes);
+// Sky or Stars
+const stars = starfields();
+solarSystemGroup.add(stars);
 
-const exempleBox = createBox();
-exempleBox.position.set(0,0,5)
-scene.add(exempleBox);
-
-
+// Image in background
+const loader = new THREE.TextureLoader();
+const backgroundImage = loader.load('https://images.pexels.com/photos/1205301/pexels-photo-1205301.jpeg');
+scene.background = backgroundImage;
 
 
 // const skyHorizont = createSkyHorizont(scene);
+const sky = createSkyHorizont();
+// scene.background = sky;
+sky.position.set(5,5,5);
+solarSystemGroup.add(sky);
+
+
+// tridimensional axes to understands the dimension
+const axes = new THREE.AxesHelper(10);
+solarSystemGroup.add(axes);
+
+// const exempleBox = createBox();
+// exempleBox.position.set(0,0,5);
+// exempleBox.color(0x00FF00);
+// scene.add(exempleBox);
+
+
+// Add sun
 const sun = createPlanet(sunTexture, 3,90,90, true);
-scene.add(sun);
+solarSystemGroup.add(sun);
 
-const sunGeometry = new THREE.SphereGeometry(5,46,99);
-const sunMaterial = new THREE.MeshBasicMaterial({ // MeshPhongMaterial // MeshStandardMaterial // MeshBasicMaterial
-    map: loader.load(sunTexture)
-});
-const sun1 = new THREE.Mesh(sunGeometry, sunMaterial);
-scene.add(sun1);
+// const imgLoader = new THREE.ImageLoader();
+// const sun = createPlanet(sunTexture, 3,90,90, true);
+// scene.add(sun);
 
-
-
-
+scene.add(solarSystemGroup);
 function animate() {
-  exempleBox.rotation.x += 0.01;
-  exempleBox.rotation.y += 0.01;
+  // exempleBox.rotation.x += 0.01;
+  // exempleBox.rotation.y += 0.01;
   stars.rotation.y -= 0.0002;
   sun.rotateY(0.009);
   rendered.render(scene, camera);
 }
 
 //timeline as magic to apears 
-// const timeLinescale = gsap.timeline({ defaults: { duration: 1}});
-// timeLinescale.fromTo(mesh.scale, {z:0, x:0, y:0}, {z:1, x:1, y:1});
-// timeLinescale.fromTo("nav", {y:"-100%"}, {y:"0%"}); // to a nav bar
+const timeLinescale = gsap.timeline({ defaults: { duration: 3}});
+timeLinescale.fromTo(solarSystemGroup.scale, {z:0, x:0, y:0}, {z:1, x:1, y:1});
+timeLinescale.fromTo("nav", {y:"-100%"}, {y:"0%"}); // to a nav bar
+
+document.addEventListener("keydown", onDocumentKeyDown, false);
+function onDocumentKeyDown(event) {
+  var keyCode = event.which;
+  if (keyCode == 87) {
+    // up
+    sun.position.y += 1;  
+  } else if (keyCode == 83) {
+    // down
+    sun.position.y -= 1;
+  } else if (keyCode == 65) {
+    // left
+    sun.position.x -= 1;
+  } else if (keyCode == 68) {
+    // right
+    sun.position.x += 1;
+  } else if (keyCode == 69) {
+    // deep
+    sun.position.z += 1;
+  } else if (keyCode == 81) {
+    // undeep
+    sun.position.z -= 1;
+  } else if (keyCode == 32) {
+    // space => reset position
+    sun.position.x = 0.0;
+    sun.position.y = 0.0;
+  }awd
+  rendered();
+};
+
+
 
 // const resizer = new Resizer(container, camera, renderer);  
 window.addEventListener('resize', function(){
-    camera.aspect = window.innerWidth / this.window.innerHeight;
-    camera.updateProjectMatrix();
-    rendered.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / this.window.innerHeight;
+  camera.updateProjectMatrix();
+  rendered.setSize(window.innerWidth, window.innerHeight);
 });    
 rendered.setAnimationLoop(animate);
